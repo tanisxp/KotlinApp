c9f9b7629ac340226ab34e49bfeb406dcfc7ec88
tanisxp <tanisxp@gmail.com>
2025-10-14 13:24:33 +0200

    feat: añade modelo de datos para verbos irregulares

---
+++ app/src/main/java/com/example/myapplication/data/IrregularVerb.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.data
+
+data class IrregularVerb(
+    val infinitive: String,
+    val simplePast: String,
+    val pastParticiple: String,
+    val translation: String
+)
